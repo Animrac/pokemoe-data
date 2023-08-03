ee90368856506b52131bc3078ccda3dfac79b724
Carmina <carminacruz08@yahoo.com>
2023-08-03 01:09:48 -0700

    added colors, logo, sub heading, the special e,

--- client/src/App.js
+++ client/src/App.js
@@ -1,129 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import { Buffer } from 'buffer';
-
-// function App() {
-//   const [data, setData] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [selectedPokemon, setSelectedPokemon] = useState(null); // State to store the selected Pokemon
-
-//   useEffect(() => {
-//     // Fetch data from the /pokes3 endpoint
-//     fetch('/pokes3')
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         console.log(response);
-//         return response.json(); // Parse the response body as JSON
-//       })
-//       .then(data => {
-//         console.log(data);
-//         setData(data.poke);
-//       })
-//       .catch(error => {
-//         console.log('js so fun');
-//         setError(error.message);});
-//   }, []);
-
-//   if (error) {
-//     return <div>Error: {error}</div>;
-//   }
-
-//   const filteredData = data.filter(pokemon => {
-//     const searchTerm = searchQuery.toLowerCase();
-//     return (
-//       pokemon.name.toLowerCase().includes(searchTerm) ||
-//       pokemon.national_id.toString().includes(searchTerm)
-//     );
-//   });
-
-//   return (
-//       <div className="App" style={{ display: 'flex' }}>
-//         {/* Left Container */}
-//         <div style={{ width: '350px', textAlign: 'center' }}>
-//           <h1>Pokemon Data</h1>
-//           <div style={{ padding: '20px' }}>
-//             <input
-//               type="text"
-//               placeholder="Search by name or national ID"
-//               value={searchQuery}
-//               onChange={e => setSearchQuery(e.target.value)}
-//               style={{ width: '100%' }}
-//             />
-//           </div>
-//           {filteredData.length > 0 ? (
-//             <div className="scroll-container">
-//               <ul style={{ margin: 0, padding: 0 }}>
-//                 {filteredData.map(pokemon => (
-//                   <li key={pokemon.national_id} style={{ margin: '0', padding: '0', marginBottom: '0px' }}>
-//                     <button
-//                       onClick={e => setSelectedPokemon(pokemon)} // Add a click event handler
-//                       style={{
-//                         display: 'flex',
-//                         alignItems: 'center',
-//                         border: 'none',
-//                         background: 'none',
-//                         cursor: 'pointer',
-//                         padding: 0,
-//                       }}
-//                     >
-//                       <img
-//                         src={`data:image/png;base64,${Buffer.from(pokemon.icon_1.data).toString('base64')}`}
-//                         alt={`${pokemon.name} Icon`}
-//                         style={{ width: '32px', height: '32px', marginRight: '10px', marginLeft: '20px' }}
-//                       />
-//                       <h2>{pokemon.national_id + ': ' + pokemon.name}</h2>
-//                     </button>
-//                   </li>
-//                 ))}
-//               </ul>
-//             </div>
-//           ) : (
-//             <p>No matching Pokemon found.</p>
-//           )}
-//         </div>
-        
-//       {/* Right Container */}
-//       <div style={{ width: '100%', textAlign: 'center', padding: '20px', backgroundColor: '#f0f0f0' }}>
-//         {/* Your content for the right container */}
-//         {/* <h2>Right Container</h2>
-        
-//         <p>This is the content for the right container.</p> */}
-        
-//         {/* Display the name of the clicked Pokemon */}
-//         {selectedPokemon ? (
-//           <div>
-//             <h2>Selected Pokemon</h2>
-//             <img
-//               src={`data:image/png;base64,${Buffer.from(selectedPokemon.sprite.data).toString('base64')}`}
-//               alt={`${selectedPokemon.name} Sprite`}
-//               style={{ width: '64px', height: '64px'}}
-//             />
-//             <p>{selectedPokemon.name}</p>
-//           </div>
-//         ) : (
-//           <h2>No Pokemon selected</h2>
-//         )}
-//         </div>
-//       </div>
-//   );
-// }  
-
-// // const handleButtonClick = (pokemon) => {
-// //   // Here, you can perform the desired action with the clicked Pokemon data
-// //   // alert(`You clicked on ${pokemon.name}.`);
-  
-// //   value={pokemon}
-// //   setSelectedPokemon(pokemon.name);
-// // };
-
-
-
 // export default App;
 
 import React, { useState, useEffect } from 'react';
 import { Buffer } from 'buffer';
+import logo from './moe.png';
 
 function App() {
   const [data, setData] = useState([]);
@@ -176,22 +55,47 @@ function App() {
   return (
     <div className="App" style={{ display: 'flex' }}>
       {/* Left Container */}
-      <div style={{ width: '350px', textAlign: 'center' }}>
-        <h1>Pokemon Data</h1>
-        <div style={{ padding: '20px' }}>
+      <div style={{ width: '400px', textAlign: 'center' }}>
+
+
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+  <div>
+    <img
+      src={logo}
+      alt={`Logo`}
+      style={{ width: '120px' }}
+    />
+  </div>
+  <div style={{ marginLeft: '10px' }}>
+    <h1 style={{ fontSize: '24px' }}>PokéMoe Data</h1>
+    <h2 style={{ fontSize: '16px' }}>&lt;3</h2>
+  </div>
+        </div>
+        <div style={{ padding: '20px', marginTop: '-25px' }}>
           <input
             type="text"
             placeholder="Search by name or national ID"
             value={searchQuery}
             onChange={e => setSearchQuery(e.target.value)}
-            style={{ width: '100%' }}
+            style={{
+              width: '100%', // Change the width of the search rectangle here
+              height: '40px', // Change the height of the search rectangle here
+              fontSize: '16px', // Change the font size of the text inside the input
+              fontWeight: 'bold'
+            }}
+
           />
         </div>
         {filteredData.length > 0 ? (
           <div className="scroll-container">
             <ul style={{ margin: 0, padding: 0 }}>
-              {filteredData.map(pokemon => (
-                <li key={pokemon.national_id} style={{ margin: '0', padding: '0', marginBottom: '0px' }}>
+              {filteredData.map((pokemon, index) => (
+                <li key={pokemon.national_id} style={{ 
+                margin: '0', 
+                padding: '0', 
+                marginBottom: '0px',
+                backgroundColor: index % 2 === 0 ? '#f3fce3' : '#ffffff',
+                width: '100%' }}>
                   <button
                     onClick={() => fetchSprite(pokemon.national_id)} // Fetch sprite when button is clicked
                     style={{
@@ -201,6 +105,7 @@ function App() {
                       background: 'none',
                       cursor: 'pointer',
                       padding: 0,
+                      width: '100%'
                     }}
                   >
                     <img
@@ -215,7 +120,7 @@ function App() {
             </ul>
           </div>
         ) : (
-          <p>No matching Pokemon found.</p>
+          <p>No matching Pokémon found.</p>
         )}
       </div>
 
@@ -224,7 +129,7 @@ function App() {
         {/* Your content for the right container */}
         {selectedPokemon ? (
           <div>
-            <h2>Selected Pokemon</h2>
+            <h2>Selected Pokémon</h2>
             <img
               src={`data:image/png;base64,${Buffer.from(selectedPokemon.sprite.data).toString('base64')}`}
               alt={`${selectedPokemon.name} Sprite`}
@@ -233,7 +138,7 @@ function App() {
             <p>{selectedPokemon.name}</p>
           </div>
         ) : (
-          <h2>No Pokemon selected</h2>
+          <h2>No Pokémon selected</h2>
         )}
       </div>
     </div>
